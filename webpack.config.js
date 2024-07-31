d8e1d5c3614e5d9ca506211206bc4386f56bdc25
mimzeslami <mohammadrezaeslami75@gmail.com>
2024-07-31 17:10:43 +0330

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,6 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     plugins: [new TsconfigPathsPlugin()],
-    alias: {
-      components: path.resolve(__dirname, "src/components/"),
-      contexts: path.resolve(__dirname, "src/contexts/"),
-      pages: path.resolve(__dirname, "src/pages/"),
-      routes: path.resolve(__dirname, "src/routes/"),
-      services: path.resolve(__dirname, "src/services/"),
-      styles: path.resolve(__dirname, "src/styles/"),
-      tests: path.resolve(__dirname, "src/tests/"),
-    },
   },
   module: {
     rules: [
